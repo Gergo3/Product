160cde13a534c7856371f862b0ee48403b05fd20
BuildTools <unconfigured@null.spigotmc.org>
2022-02-14 17:19:24 +0100

    adatbázis kapcsolat

--- productbackend/Controllers/ProductController.cs
+++ productbackend/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductBackend;
 using ProductBackend.Models;
 
 namespace productbackend.Controllers;
@@ -7,6 +9,44 @@ namespace productbackend.Controllers;
 [Route("[controller]")]
 public class ProductController : ControllerBase
 {
+    [HttpPost("RecreateDatebase")]
+    public async Task RecreateDatebase()
+    {
+        using var db = new ProductContext();
+        db.Database.EnsureDeleted();
+        await db.Database.MigrateAsync();
+        
+    }
+
+    [HttpPost("Populate")]
+    public async Task Populate()
+    {
+        var products = new List<Product>();
+        var product1 = new Product() { Id = Guid.NewGuid(), Name = "Alma", Price = 7.14, Rating = 5, Active = true, };
+        products.Add(product1);
+        var product2 = new Product() { Id = Guid.NewGuid(), Name = "Körte", Price = 4.84, Rating = 8, Active = true, };
+        products.Add(product2);
+        var product3 = new Product() { Id = Guid.NewGuid(), Name = "Barack", Price = 5, Rating = 2, Active = true, };
+        products.Add(product3);
+
+        using var db = new ProductContext();
+        db.AddRange(products);
+        var product4 = new Product() { Id = Guid.NewGuid(), Name = "Szõlõ", Price = 2, Rating = 23, Active = false, };
+        db.Add(product4);
+
+        await db.SaveChangesAsync();
+    }
+    [HttpGet("Get2")]
+    public List<Product> Get2(string search)
+    {
+        using var db = new ProductContext();
+        var res2 = db.Products
+            .Where(x => x.Name.ToLower().Contains(search.ToLower()))
+            .Where(x => x.Active)
+            .OrderByDescending(x => x.Rating)
+            .ToList();
+        return res2;
+    }
 
     [HttpGet]
     public List<Product> Get(string search)
---
+++ productbackend/ProductContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ProductBackend;
+
+public class ProductContext : DbContext
+{
+    private string ConnectionString { get; set; }
+
+    public DbSet<Models.Product> Products { get; set; } = null!;
+
+
+    public ProductContext()
+    {
+
+        ConnectionString = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_NexxtRdb") ?? "";
+        if (string.IsNullOrWhiteSpace(ConnectionString))
+            throw new Exception("Connection string is empty");
+        var contextName = this.GetType().Name;
+        var databaseName = contextName.Remove(contextName.Length - "Context".Length);
+        ConnectionString += ";Database=" + databaseName;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options
+            .UseNpgsql(ConnectionString);
+
+    }
+}
